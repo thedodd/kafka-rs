8835af23fcaca5e33386956e1a473445a16a29d9
Anthony Dodd <dodd.anthonyjosiah@gmail.com>
2024-03-05 20:43:05 -0600

    Implement high-level topic producer
    Core client is responsible for managing the lifecycle of cluster
    bootstrapping, API versions, metadata discovery, connecting to new
    brokers, reconnecting upon transient errors, and so on.
    
    The core client is a series of tasks which cooperate together to provide
    a uniform, topic-oriented interface to the backing Kafka cluster.
    
    For every topic that an app needs to publish to, a TopicProducer may be
    created which multiplexes all of its broker interaction over the same
    TCP connections of the core client.
    
    The TopicProducer exclusively uses the uniform sticky producer strategy,
    for performance, atomicity, efficiency, and simplicity. The target
    partition is rotated after every interaction to ensure distribution of
    load across partitions.

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,10 @@ mod client;
 mod clitask;
 mod codec;
 
-pub use client::Client;
-pub use kafka_protocol as proto;
+pub use kafka_protocol;
+pub use kafka_protocol::indexmap;
+
+pub use client::{Acks, Client, Message};
+pub use kafka_protocol::indexmap::IndexMap;
+pub use kafka_protocol::protocol::StrBytes;
+pub use kafka_protocol::records::Compression;
